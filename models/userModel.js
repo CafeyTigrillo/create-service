ec62bb6edd6baad951a47a7f5fd9b2fcc27a409d
Edgar <epgarzonr@uce.edu.ec>
2025-01-23 05:31:29 -0500

    feat: Add user creation functionality with validation and unique identifiers

---
+++ models/userModel.js
@@ -0,0 +1,52 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Users = sequelize.define(
+  'Users',
+  {
+    user_id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      allowNull: false,
+      unique: true,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    branch_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+
+module.exports = { Users };
