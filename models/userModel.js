c5f3e15c61b5a52e279753332c4902b75e79c889
Edgar <epgarzonr@uce.edu.ec>
2025-02-01 19:25:39 -0500

    Resolve merge conflict in user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
@@ -51,57 +50,3 @@ const Users = sequelize.define(
 );
 
 module.exports = { Users };
-=======
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
-
-const Users = sequelize.define(
-  'Users',
-  {
-    user_id: {
-      type: DataTypes.STRING,
-      primaryKey: true,
-      allowNull: false,
-      unique: true,
-    },
-    first_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    last_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
-      },
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    branch_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    created_at: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
-    },
-  },
-  {
-    timestamps: false,
-  }
-);
-
-module.exports = { Users };
->>>>>>> test
