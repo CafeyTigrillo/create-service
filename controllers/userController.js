5d63f41e2c9bec3d634d47f62ccf3174bfd7f0c0
Edgar <epgarzonr@uce.edu.ec>
2025-02-05 07:02:09 -0500

    Add Swagger documentation for the user registration API endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcryptjs");
-const User = require("../models/User");
+const User = require("../models/usermodel");
 
 const registerUser = async (req, res) => {
   try {
--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const express = require("express");
 const sequelize = require("./config/database");
 const userRoutes = require("./routes/userRoutes");
 const cors = require('cors');  
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./docs/swagger.yaml");  
 
 const app = express();
 app.use(express.json());
 app.use(cors());  // This will allow all CORS requests
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/users", userRoutes);
 
