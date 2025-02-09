cf61974b6785fe09b7e4e56efeaed5ada5de367b
Edgar <epgarzonr@uce.edu.ec>
2025-02-09 10:49:06 -0500

    Added Eureka client connection and resolved folder conflicts

--- server.js
+++ server.js
@@ -5,21 +5,53 @@ const cors = require('cors');
 const swaggerUi = require("swagger-ui-express");
 const YAML = require("yamljs");
 const swaggerDocument = YAML.load("./docs/swagger.yaml");  
+const Eureka = require('eureka-js-client').Eureka;
 
 const app = express();
+
+
+const eurekaClient = new Eureka({
+  instance: {
+    app: 'createEmployees-service',  
+    hostName: 'localhost',
+    ipAddr: '127.0.0.1',
+    port: {
+      '$': process.env.PORT || 3001,
+      '@enabled': true,
+    },
+    vipAddress: 'users-service',
+    dataCenterInfo: {
+      '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+      name: 'MyOwn',
+    },
+    registerWithEureka: true,
+    fetchRegistry: true,
+  },
+  eureka: {
+    host: 'localhost',  
+    port: 8761,     
+    servicePath: '/eureka/apps/',
+  },
+});
+
 app.use(express.json());
-app.use(cors());  // This will allow all CORS requests
+app.use(cors());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
 app.use("/users", userRoutes);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, async () => {
   try {
     await sequelize.sync({ force: false });
+    eurekaClient.start();
     console.log(`Server running at http://localhost:${PORT}`);
   } catch (error) {
     console.error("Error connecting to the database:", error);
   }
+});
+
+process.on('SIGINT', () => {
+  eurekaClient.stop();
+  process.exit();
 });
